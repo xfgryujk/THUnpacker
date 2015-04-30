bd49eaaf3da3b98f077f38dee66e1e6d59f03399
noname00 <xfgryujk@126.com>
2015-04-30 23:13:27 +0800

    Support TH06

--- THUnpacker/THUnpacker.cpp
+++ THUnpacker/THUnpacker.cpp
@@ -2,6 +2,118 @@
 #include "common.h"
 
 
+int unpackTH06(FILE* _f)
+{
+	class PBG3File
+	{
+	protected:
+		FILE* f;
+		BYTE buffer;
+		BYTE mask;
+
+	public:
+		PBG3File(FILE* _f)
+		{
+			f = _f;
+			mask = 0;
+		}
+
+		void setPointer(DWORD address)
+		{
+			fseek(f, address, SEEK_SET);
+			mask = 0;
+		}
+
+		BOOL read1Bit()
+		{
+			if (mask == 0)
+			{
+				fread(&buffer, 1, 1, f);
+				mask = 1 << 7;
+			}
+			BOOL result = (buffer & mask) != 0;
+			mask >>= 1;
+			return result;
+		}
+
+		DWORD readBits(DWORD bitsToRead)
+		{
+			DWORD result = 0;
+			for (DWORD resultMask = 1 << (bitsToRead - 1); resultMask != 0; resultMask >>= 1)
+				if (read1Bit())
+					result |= resultMask;
+			return result;
+		}
+
+		DWORD readNumber()
+		{
+			DWORD size = readBits(2) + 1;
+			return readBits(size * 8);
+		}
+
+		void readString(char* strBuffer, DWORD size)
+		{
+			DWORD i;
+			for (i = 0; i < size; i++)
+				if ((strBuffer[i] = readBits(8)) == '\0')
+					break;
+			if (i >= size)
+				strBuffer[size - 1] = '\0';
+		}
+	} f(_f);
+	
+	// read header
+	DWORD count = f.readNumber();
+	DWORD indexAddreass = f.readNumber();
+
+	// check
+	if (count <= 0)
+	{
+		puts(E_FILE_COUNT);
+		return EN_FILE_COUNT;
+	}
+
+	DWORD fileSize = getFileSize(_f);
+	if (fileSize <= indexAddreass)
+	{
+		puts(E_FILE_SIZE);
+		return EN_FILE_SIZE;
+	}
+
+	// read index
+	f.setPointer(indexAddreass);
+	vector<Index> index(count);
+	for (Index& i : index)
+	{
+		// we don't use them
+		f.readNumber();
+		f.readNumber();
+		f.readNumber();
+
+		i.address =	f.readNumber();
+		i.originalLength = f.readNumber();
+		i.name.resize(256);
+		f.readString(const_cast<char*>(i.name.c_str()), 256);
+		i.name.resize(strlen(i.name.c_str()));
+
+		printf("%30s  %10d  %10d\n", i.name.c_str(), i.address, i.originalLength);
+	}
+	DWORD i;
+	for (i = 0; i < count - 1; i++)
+		index[i].length = index[i + 1].address - index[i].address;
+	index[i].length = indexAddreass - index[i].address;
+
+	// export files
+	if (!exportFiles(_f, &index, "th06"))
+	{
+		puts(E_EXPORT);
+		return EN_EXPORT;
+	}
+
+	puts("done.");
+	return 0;
+}
+
 int unpackTH07(FILE* f)
 {
 	// read header
@@ -11,15 +123,15 @@ int unpackTH07(FILE* f)
 	// check
 	if (header[0] <= 0)
 	{
-		puts("Invalid file count!");
-		return 3;
+		puts(E_FILE_COUNT);
+		return EN_FILE_COUNT;
 	}
 
 	DWORD fileSize = getFileSize(f);
 	if (fileSize <= header[1])
 	{
-		puts("Invalid file size!");
-		return 4;
+		puts(E_FILE_SIZE);
+		return EN_FILE_SIZE;
 	}
 
 	// read index
@@ -41,8 +153,8 @@ int unpackTH07(FILE* f)
 	if (!exportFiles(f, index, "th07"))
 	{
 		delete index;
-		puts("Failed to export files!");
-		return 5;
+		puts(E_EXPORT);
+		return EN_EXPORT;
 	}
 	delete index;
 
@@ -66,15 +178,15 @@ int unpackTH08(FILE* f)
 	// check
 	if (header[0] <= 0)
 	{
-		puts("Invalid file count!");
-		return 3;
+		puts(E_FILE_COUNT);
+		return EN_FILE_COUNT;
 	}
 
 	DWORD fileSize = getFileSize(f);
 	if (fileSize <= header[1])
 	{
-		puts("Invalid file size!");
-		return 4;
+		puts(E_FILE_SIZE);
+		return EN_FILE_SIZE;
 	}
 
 	// read index
@@ -100,8 +212,8 @@ int unpackTH08(FILE* f)
 	if (!exportFiles(f, index, "th08"))
 	{
 		delete index;
-		puts("Failed to export files!");
-		return 5;
+		puts(E_EXPORT);
+		return EN_EXPORT;
 	}
 	delete index;
 
@@ -122,8 +234,8 @@ int main(int argc, char* argv[])
 	fopen_s(&f, argv[1], "rb");
 	if (f == NULL)
 	{
-		puts("Failed to open the file!");
-		return 1;
+		puts(E_OPEN_FILE);
+		return EN_OPEN_FILE;
 	}
 
 	// check magic number
@@ -132,6 +244,9 @@ int main(int argc, char* argv[])
 	int result = 0;
 	switch (magicNumber)
 	{
+	case 0x33474250: // "PBG3" for TH06
+		result = unpackTH06(f);
+		break;
 	case 0x34474250: // "PBG4" for TH07
 		result = unpackTH07(f);
 		break;
@@ -139,8 +254,8 @@ int main(int argc, char* argv[])
 		result = unpackTH08(f);
 		break;
 	default:
-		puts("Unknown file type!");
-		result = 2;
+		puts(E_UNKNOWN_TYPE);
+		result = EN_UNKNOWN_TYPE;
 		break;
 	}
 
--- THUnpacker/common.h
+++ THUnpacker/common.h
@@ -27,3 +27,14 @@ struct Index
 };
 vector<Index>* formatIndex(const BYTE* indexBuffer, DWORD fileCount, DWORD indexAddress);
 BOOL exportFiles(FILE* f, const vector<Index>* index, string dirName);
+
+const char E_OPEN_FILE[]		= "Failed to open the file!";
+const int EN_OPEN_FILE			= 2;
+const char E_UNKNOWN_TYPE[]		= "Unknown file type!";
+const int EN_UNKNOWN_TYPE		= 3;
+const char E_FILE_COUNT[]		= "Invalid file count!";
+const int EN_FILE_COUNT			= 4;
+const char E_FILE_SIZE[]		= "Invalid file size!";
+const int EN_FILE_SIZE			= 5;
+const char E_EXPORT[]			= "Failed to export files!";
+const int EN_EXPORT				= 6;
